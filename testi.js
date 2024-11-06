1c9a293cd1245051bcdf72e43c8dcc9e1f59b993
TopiAhola <topi.ahola@metropolia.fi>
2024-11-06 09:46:02 +0200

    Testi nucilta 1

---
+++ testi.js
@@ -0,0 +1,13 @@
+/*
+
+
+*/
+
+
+'use strict';
+
+console.log("Im printing to console");
+alert("ponnahdusikkuna!");
+
+const tuloste = "tulostetaan tämä";
+document.querySelector('#tulostusalue1').innerHTML = tuloste;
