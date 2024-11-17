9d924ad14fa0696d8fd47757bbddef72b7c33914
TopiAhola <topi.ahola@metropolia.fi>
2024-11-17 19:50:16 +0200

    Moduuli 4 tehtävä 1 nuc

--- JavaScript Tehtävät/JavaScript_Muistiinpanoja.js
+++ JavaScript Tehtävät/JavaScript_Muistiinpanoja.js
@@ -121,3 +121,21 @@ form.addEventListener('submit', function(event) {
   console.log (event);
 })
 // .preventDefault(); estää konsolia nollautumasta.
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Asynchronous fuctions
+
+async function asynchronousFunction() {                 // asynchronous function is defined by the async keyword
+        console.log('asynchronous download begins');
+        try {                                               // error handling: try/catch/finally
+            const response = await fetch('http://127.0.0.1:3000/airport/00A');    // starting data download, fetch returns a promise which contains an object of type 'response'
+            const jsonData = await response.json();          // retrieving the data retrieved from the response object using the json() function
+            console.log(jsonData.ICAO, jsonData.Name);    // log the result to the console
+        } catch (error) {
+            console.log(error.message);
+        } finally {                                         // finally = this is executed anyway, whether the execution was successful or not
+            console.log('asynchronous load complete');
+        }
+    }
+
+
