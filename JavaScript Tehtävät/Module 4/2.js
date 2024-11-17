9555a4614aecfa624beefedaf06718e0ef3f1bc3
TopiAhola <topi.ahola@metropolia.fi>
2024-11-18 00:14:17 +0200

    Moduuli 4 tehtävä 1 nuc

--- JavaScript Tehtävät/Module 4/2.js
+++ JavaScript Tehtävät/Module 4/2.js
@@ -1,19 +1,15 @@
 'use strict';
-let moduuli = "Moduuli ";
-let tehtava = "Tehtävä ";
+let moduuli = "Moduuli 4";
+let tehtava = "Tehtävä 2";
 let tehtavananto = `
-
+Develop the app further.
+Add JavaScript that gets the value entered to the form and sends a request with fetch to https://api.tvmaze.com/search/shows?q=${value_from_input}. Print the search result to the console. (3p)
 `
 
 
 
 
-
-
-const tuloste = "tulostetaan tämä";
-
 //Tulostetaan moduuli, tehtävän numero ja tuloste sivulle:
 document.querySelector(`#moduuli`).innerHTML = moduuli;
 document.querySelector('#tehtava').innerHTML = tehtava;
 document.querySelector('#tehtavananto').innerHTML = tehtavananto;
-document.querySelector('#tuloste').innerHTML = tuloste;
